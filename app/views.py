2748c7f68dca47de14de7a0ed1048dd81e6fd9f1
Dwarven Shpere Jast <djast@student.21-school.ru>
2021-10-13 17:19:24 +0300

    [ADD] Send file and get information about file

---
+++ app/views.py
@@ -0,0 +1,29 @@
+import fileinput
+import zipfile
+
+from flask import request
+
+from app import app
+
+
+@app.route('/', methods=['GET'])
+def getInformation():
+    return 'HELLO!'
+    pass
+
+
+@app.route('/', methods=['POST'])
+def postZIP():
+    if 'file' not in request.files:
+        return {'status': 'ERROR', 'description': 'No file'}
+    file = request.files['file']
+    if file.filename.rsplit('.', 1)[1] != 'zip':
+        return {'status': 'ERROR', 'description': "It's not a .ZIP file"}
+    sended_zip_file = zipfile.ZipFile(file)
+    result = {'status': 'SUCCESS', 'response': {'file': file.filename, 'content': []}}
+    for filename in sended_zip_file.namelist():
+        file_info = sended_zip_file.getinfo(filename)
+        result['response']['content'].append({'path': filename, 'size': int(file_info.file_size)})
+    #sended_zip_file.namelist()
+    return result
+    pass
